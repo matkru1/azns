7af595f9fbcf6e08388635a990349fb3420c741e
unknown <Peja@.(none)>
2013-05-10 17:19:06 +0200

    nowy z twitterem

--- radio.php
+++ radio.php
@@ -13,7 +13,7 @@ standby="Loading Microsoft Windows Media Player components..." type="application
 
 <EMBED type="application/x-mplayer2" pluginspage="http://microsoft.com/windows/mediaplayer/en/download/"
 id="mediaPlayer" name="mediaPlayer" showcontrols="false" width="480" height="240"
-src="http://195.150.20.244:84/RMFFM48" autostart="true" loop="true"></EMBED>
+src="http://195.150.20.244:84/RMFFM48" autostart="false" loop="true"></EMBED>
 </OBJECT>
 
 
---
+++ twitter/config.php
@@ -0,0 +1,5 @@
+<?php
+define('CONSUMER_KEY', 'OyplEQjLvJ66a2S1y7gfyQ');
+define('CONSUMER_SECRET', 'zHQ91nyWDctWfb198k0z0KSP4mOwT5yKWqrNej0oaGU');
+define('OAUTH_CALLBACK', 'http://absurdy.eu/process.php');
+?>
